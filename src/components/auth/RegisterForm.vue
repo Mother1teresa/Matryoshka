c99a9378a160fd18f11de6601680351ed82bc624
Mother1teresa <129117416+Mother1teresa@users.noreply.github.com>
2026-08-25 14:34:05 +0300

    update link vk style

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -32,10 +32,8 @@
     <div class="form-using">
       <div class="form-using__title">Продолжить через</div>
       <button type="button" class="vk-btn" @click="redirectToVK">
-        <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12.785 16.241s.288-.032.436-.194c.136-.148.132-.427.132-.427s-.02-1.304.587-1.496c.598-.19 1.365 1.26 2.18 1.817.616.422 1.084.33 1.084.33l2.177-.03s1.14-.071.599-.97c-.044-.073-.314-.66-1.617-1.866-1.364-1.261-1.182-1.057.462-3.236.998-1.332 1.397-2.145 1.272-2.494-.12-.333-.86-.245-.86-.245l-2.45.015s-.182-.025-.316.056c-.132.08-.216.266-.216.266s-.39 1.037-.91 1.92c-1.096 1.86-1.534 1.96-1.713 1.842-.418-.27-.314-1.085-.314-1.663 0-1.808.274-2.562-.534-2.758-.268-.065-.465-.108-1.148-.115-.876-.01-1.618.003-2.038.208-.28.138-.495.444-.364.462.163.022.532.1.728.363.253.34.244 1.103.244 1.103s.145 1.063-.34 1.195c-.333.09-.704-.093-1.576-.93-1.116-1.074-1.575-2.145-1.575-2.145s-.13-.284-.363-.437c-.284-.19-.68-.249-.68-.249l-2.33.015s-.35.01-.478.162c-.115.137-.01.42-.01.42s1.827 4.273 3.897 6.43c1.898 1.977 4.054 1.85 4.054 1.85h.977z" fill="#fff"/>
-        </svg>
-        <span>ВКонтакте</span>
+        <img src="/src/assets/img/form/vk-form.svg" alt="">
+        <!-- <span>ВКонтакте</span> -->
       </button>
     </div> 
     <div class="form-noaccount">
@@ -204,15 +202,12 @@ async function redirectToVK() {
   display: flex;
   align-items: center;
   gap: 0.625rem;
-  background: #0077ff;
   color: #fff;
   border: none;
-  border-radius: 0.75rem;
-  padding: 0.5rem 1.25rem;
-  font-size: 1rem;
   cursor: pointer;
   transition: opacity 0.3s;
 }
+.vk-btn img{ width: 1.875rem; height: 1.875rem; }
 .vk-btn:hover { opacity: 0.85; }
 .btn-noaccount {
   color: #6e6e6e;
@@ -243,28 +238,4 @@ async function redirectToVK() {
   margin-top: 0.313rem;
   margin-bottom: 0.75rem;
 }
-/* .vk-auth-container {
-  display: flex;
-  justify-content: center;
-  margin: 8px 0;
-}
-.vk-auth-container :deep(iframe) {
-  max-width: 100% !important;
-}
-.vk-wrapper {
-  width: 20%;
-  display: block;
-}
-#hoezgo .VkIdWebSdk__button_hoezgo{
-  width: 2.5rem !important;
-  height: 2.5rem !;
-  padding: 0.375rem !important;
-  border-radius: 0.75rem !important;
-}
-#xekcyg .VkIdWebSdk__button_xekcyg{
-  width: 2.5rem !important;
-  height: 2.5rem !;
-  padding: 0.375rem !important;
-  border-radius: 0.75rem !important;
-} */
 </style>
